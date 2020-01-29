dcba8b78ad6d75a217b24dbe6e617234a5e74a0d
Daniel-Pivonka <dpivonka@redhat.com>
2020-01-29 11:38:26 -0500

    comments

--- spr2020/class2/examples/bouncebutton.py
+++ spr2020/class2/examples/bouncebutton.py
@@ -6,21 +6,24 @@ GPIO.setmode(GPIO.BCM)
 #setup pin as input
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-
+#var
 clickedflag = False
 
 #loop endlessly
 while True:
 
 		#get button state
-        switchstate = GPIO.input(21)
+		buttonstate = GPIO.input(21)
 
-
-		if (buttonstate and not clickedflag)
+		#if the button is pressed and was not previously clicked
+		if (buttonstate and not clickedflag):
+			#set as clicked
 			clickedflag = True;
 
+		#if button is not clicked and was previously not clicked 
+		if (not buttonstate and clickedflag):
+			#set as not clicked
+			clickedflag = False;
 
-  
-  		if (not buttonstate and clickedflag)
-    		clickedflag = False;
+			#print
 			print ('clicked')
